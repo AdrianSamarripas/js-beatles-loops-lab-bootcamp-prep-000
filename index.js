4f65fb3f85d8fdc7e076000e43c1e6d617a2656d
Adrian Samarripas <asamarripas@gmail.com>
2018-05-08 09:18:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   var newarray = [];
-  
+  for (let i = 0; i > musicians.length; i++){
+    newarray.push(`${musicians} plays ${instruments}`);
+  }
+  return newarray;
 }
